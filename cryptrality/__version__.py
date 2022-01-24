4fa924d5ad19158d4099636a19bb5bea98855746
Francesco Favero <favero.francesco@gmail.com>
2022-01-24 11:55:44 +0100

    Delete old things

--- cryptrality/__version__.py
+++
@@ -1,8 +0,0 @@
-from modular_bot.setup import WEBSITE
-
-
-VERSION =0.01
-AUTHOR = 'Francesco'
-DATE = '09/11/2021'
-MAIL = 'favero.francesco@gmail.com'
-WEBSITE = ''
--- scripts/README.md
+++
@@ -1,4 +0,0 @@
-
-### Download files for the entire 2020
-
-    python3 download_yearly_data.py MATICUSDT 2020 4h
